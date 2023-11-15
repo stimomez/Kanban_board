c4ad8dc813c3cc44880891d8333f298ef72f0e07
Stiven_Morales <stimomez@gmail.com>
2023-11-15 00:36:12 -0500

    feat: Persistencia de datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,116 +1,156 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import DragAndDrop from "./components/DragAndDrop";
 
 function App() {
+  const colors = {
+    violet: "#6a6dcd",
+    fuchsia: "#e3a7d3",
+    red: "#d93535",
+    blue: "#3180e2",
+    green: "#00a88b",
+  };
   const [tasks, setTasks] = useState<Tasks[]>([
-    {
-      id: 1,
-      title: "Tarea 1",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 1,
-    },
-    {
-      id: 2,
-      title: "Tarea 2",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 2,
-    },
-    {
-      id: 3,
-      title: "Tarea 3",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 2,
-    },
-    {
-      id: 4,
-      title: "Tarea 4",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 3,
-    },
-    {
-      id: 5,
-      title: "Tarea 5",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 6,
-      title: "Tarea 6",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 7,
-      title: "Tarea 7",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 8,
-      title: "Tarea 8",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 9,
-      title: "Tarea 9",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 10,
-      title: "Tarea 10",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 11,
-      title: "Tarea 11",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 12,
-      title: "Tarea 12",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
-    {
-      id: 13,
-      title: "Tarea 13",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
-      list: 4,
-    },
+    // {
+    //   id: 1,
+    //   title: "Tarea 1",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 1,
+    //   color: colors.violet,
+    // },
+    // {
+    //   id: 2,
+    //   title: "Tarea 2",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 2,
+    //   color: colors.violet,
+    // },
+    // {
+    //   id: 3,
+    //   title: "Tarea 3",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 2,
+    //   color: colors.green,
+    // },
+    // {
+    //   id: 4,
+    //   title: "Tarea 4",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 3,
+    //   color: colors.violet,
+    // },
+    // {
+    //   id: 5,
+    //   title: "Tarea 5",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.green,
+    // },
+    // {
+    //   id: 6,
+    //   title: "Tarea 6",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.green,
+    // },
+    // {
+    //   id: 7,
+    //   title: "Tarea 7",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.red,
+    // },
+    // {
+    //   id: 8,
+    //   title: "Tarea 8",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.red,
+    // },
+    // {
+    //   id: 9,
+    //   title: "Tarea 9",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.blue,
+    // },
+    // {
+    //   id: 10,
+    //   title: "Tarea 10",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.blue,
+    // },
+    // {
+    //   id: 11,
+    //   title: "Tarea 11",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.fuchsia,
+    // },
+    // {
+    //   id: 12,
+    //   title: "Tarea 12",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.fuchsia,
+    // },
+    // {
+    //   id: 13,
+    //   title: "Tarea 13",
+    //   body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+    //   list: 4,
+    //   color: colors.green,
+    // },
   ]);
 
+  useEffect(() => {
+    const storedData = localStorage.getItem("taskData");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setTasks(parsedData);
+    }
+  }, []);
+  // console.log(tasks);
+
+  const saveDataLocalStorage = () =>
+    localStorage.setItem("taskData", JSON.stringify(tasks));
+
+  // useEffect(() => {
+  //   localStorage.setItem("taskData", JSON.stringify(tasks));
+  // }, [tasks]);
+
   return (
-    <div>
-      <h2>Roadmap</h2>
-      <div className="flex gap-1">
+    <div className=" w-full ">
+      <h2 className="text-3xl text-white ml-56">Roadmap</h2>
+      <div className="flex gap-1 justify-center ">
         <DragAndDrop
           title="backlog"
           tasks={tasks}
           setTasks={setTasks}
           numberList={1}
+          saveDataLocalStorage={saveDataLocalStorage}
         />
         <DragAndDrop
           title="to do"
           tasks={tasks}
           setTasks={setTasks}
           numberList={2}
+          saveDataLocalStorage={saveDataLocalStorage}
         />
         <DragAndDrop
           title="in progress"
           tasks={tasks}
           setTasks={setTasks}
           numberList={3}
+          saveDataLocalStorage={saveDataLocalStorage}
         />
         <DragAndDrop
           title="designed"
           tasks={tasks}
           setTasks={setTasks}
           numberList={4}
+          saveDataLocalStorage={saveDataLocalStorage}
         />
       </div>
     </div>
@@ -122,5 +162,6 @@ interface Tasks {
   title: string;
   body?: string;
   list: number;
+  color: string;
 }
 export default App;
--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -3,12 +3,14 @@ type PropsDragAndDrop = {
   tasks: Tasks[];
   setTasks: React.Dispatch<React.SetStateAction<Tasks[]>>;
   numberList: number;
+  saveDataLocalStorage: () => void;
 };
 interface Tasks {
   id: number;
   title: string;
   body?: string;
   list: number;
+  color: string;
 }
 
 const DragAndDrop = ({
@@ -16,6 +18,7 @@ const DragAndDrop = ({
   tasks,
   setTasks,
   numberList,
+  saveDataLocalStorage,
 }: PropsDragAndDrop) => {
   const getList = (list: number) => {
     return tasks.filter((item: Tasks) => item.list === list);
@@ -26,7 +29,7 @@ const DragAndDrop = ({
     item: { id: number }
   ) => {
     evt.dataTransfer.setData("itemId", item.id.toString());
-    // console.log(item);
+    console.log(item);
   };
 
   const draggingOver = (evt: React.DragEvent) => {
@@ -38,20 +41,32 @@ const DragAndDrop = ({
     const item = tasks.find((item) => item.id === parseInt(itemId, 10));
 
     if (item) {
+      console.log(item);
+
       item.list = newList;
 
       const newState = tasks.map((task) => {
+        // console.log(task);
+
+        // console.log(numberList);
+
         if (task.id === parseInt(itemId, 10)) return item;
         return task;
       });
+
+      // newState.forEach((state) => {
+      //   if (state.id === item.id) console.log(state.list);
+      // });
+
       setTasks(newState);
+      saveDataLocalStorage();
     }
   };
   return (
     <div className=" p-4 text-white">
       <ul
         style={{ minHeight: "20px" }}
-        className="w-60 flex flex-col  border gap-3 py-3 px-5 bg-background_secondary"
+        className="w-60 flex flex-col rounded-xl  gap-3 py-3 px-5 bg-background_secondary"
         draggable="true"
         onDragOver={(evt: React.DragEvent) => draggingOver(evt)}
         onDrop={(evt: React.DragEvent) => onDrop(evt, numberList)}
@@ -63,16 +78,32 @@ const DragAndDrop = ({
         {getList(numberList).map((item) => {
           return (
             <li
-              className="border cursor-pointer"
+              style={{ background: item.color }}
+              className=" rounded-xl cursor-pointer text-sm py-3 px-4 text-justify hover:opacity-70"
               key={item.id}
               draggable
               onDragStart={(evt) => startDrag(evt, item)}
             >
-              <h3>{item.title}</h3>
-              <p>{item.body}</p>
+              <h3 className="font-semibold">{item.title}</h3>
+              <p className="text-xs">{item.body}</p>
             </li>
           );
         })}
+        <div>
+          <input
+            className="input-new-task  bg-transparent cursor-pointer py-1 px-2 rounded-xl hover:bg-black outline-none"
+            placeholder="Create new task"
+            type="text"
+          />
+          <div className="btn-new-task flex justify-start mt-2  items-center gap-2 ">
+            <span className="cursor-pointer bg-blue-600 py-1 px-2  rounded-lg text-sm hover:opacity-90">
+              Add card
+            </span>
+            <span className="cursor-pointer py-1 px-2  rounded text-sm hover:bg-slate-700">
+              x
+            </span>
+          </div>
+        </div>
       </ul>
     </div>
   );
